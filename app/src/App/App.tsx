171c8fcf8ab8e6d9cc551a31496f8e9c00f0d746
Alexander0798 <rav89515139989@yandex.ru>
2025-03-07 09:15:30 +0300

    init

---
+++ app/src/App/App.tsx
@@ -0,0 +1,17 @@
+
+import "./styles/index.scss"
+
+
+function App() {
+
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
